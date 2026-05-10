262437bd259a7b25a4c28c77fa1bd9a8b7c78b2c
Jairus John Valdez <Jairus John Valdez@LAPTOP-Q4RK5VFS>
2026-05-10 22:36:52 +0800

    naiwan

--- src/application/InventoryController.java
+++ src/application/InventoryController.java
@@ -43,7 +43,7 @@ public class InventoryController {
 
     private final String[] CATEGORIES = {
         "Milktea", "Cream Cheese", "Iced Coffee", 
-        "Latte Series", "Hot Drinks", "Refreshers", "Meals", "Coffee"
+        "Latte Series", "Hot Drinks", "Refreshers", "Meals"
     };
 
     @FXML
